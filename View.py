5772b8a294e738cb4bca2bf000c8987c22a0cd76
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-10-24 14:46:17 -0300

    Update View.py

--- View.py
+++ View.py
@@ -1,9 +1,13 @@
-from Controller import *
-import os
+from Controller import *  # Importa todas as classes do módulo Controller
+import os  # Importa o módulo os que fornece funções para interagir com o sistema operacional
 
-sair = 0
-while sair == 0:
-    os.system("cls")
+sair = 0  # Inicializa a variável sair com 0
+
+while sair == 0:  # Inicia um loop que só será interrompido quando a variável sair for diferente de 0
+    
+    os.system("cls")  # Limpa a tela do console
+    
+    # Imprime o menu do programa
     print("    SOFTWARE DE TO-DO")
     print(" ________________________")
     print("|[1] -> ADICIONAR TAREFA |")
@@ -13,33 +17,30 @@ while sair == 0:
     print("|________________________|")
     print("")
 
-    #Convertendo o input para inteiro
-
-    menu = input("-> ") 
+    menu = input("-> ")  # Recebe a opção do usuário
     
-    
-    match menu:
-        case "1":
-            os.system("cls")
-            tarefa = input("Adicione uma TAREFA -> ")
-            adicionarTarefa = ControllerAdicionarTarefa(tarefa)
-            os.system("pause")
-        case "2":
-            os.system("cls")
-            listarTarefas = ControllerListarTarefas()
-            os.system("pause")
-        case "3":
-            os.system("cls")
-            listarTarefas = ControllerListarTarefas()
-            excluir = input("Qual o indice da tarefa que deseja excluir?\n-> ")
-            excluirTarefa = ControllerExcluirTarefa(excluir)
+    match menu:  # Verifica qual opção foi escolhida
+        case "1":  # Se a opção escolhida foi "1"
+            os.system("cls")  # Limpa a tela do console
+            tarefa = input("Adicione uma TAREFA -> ")  # Recebe a tarefa do usuário
+            adicionarTarefa = ControllerAdicionarTarefa(tarefa)  # Adiciona a tarefa
+            os.system("pause")  # Pausa a execução do programa até que o usuário pressione qualquer tecla
+        case "2":  # Se a opção escolhida foi "2"
+            os.system("cls")  # Limpa a tela do console
+            listarTarefas = ControllerListarTarefas()  # Lista as tarefas
+            os.system("pause")  # Pausa a execução do programa até que o usuário pressione qualquer tecla
+        case "3":  # Se a opção escolhida foi "3"
+            os.system("cls")  # Limpa a tela do console
+            listarTarefas = ControllerListarTarefas()  # Lista as tarefas
+            excluir = input("Qual o indice da tarefa que deseja excluir?\n-> ")  # Recebe o índice da tarefa que o usuário deseja excluir
+            excluirTarefa = ControllerExcluirTarefa(excluir)  # Exclui a tarefa
             print("\nLista:")
-            listarTarefas = ControllerListarTarefas()
-            os.system("pause")
-        case "4":
-            sair = 1
-            print("Saindo...")
-            os.system("pause")
-        case _:
-            print("Opção Invalida")
-            os.system("pause")
+            listarTarefas = ControllerListarTarefas()  # Lista as tarefas novamente
+            os.system("pause")  # Pausa a execução do programa até que o usuário pressione qualquer tecla
+        case "4":  # Se a opção escolhida foi "4"
+            sair = 1  # Altera o valor da variável sair para 1, fazendo com que o loop while seja interrompido
+            print("Saindo...")  
+            os.system("pause")  
+        case _:  
+            print("Opção Invalida")  
+            os.system("pause")  
