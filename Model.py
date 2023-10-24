cd09cb65ba1efcba679a512502ce052947eafbaa
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-10-24 14:45:36 -0300

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,17 +1,16 @@
-class ToDo():
-    def __init__(self):
-        self.lista = []
+class ToDo():  # Define a classe ToDo
+    def __init__(self):  # Método construtor sem parâmetros
+        self.lista = []  # Inicializa uma lista vazia para armazenar as tarefas
 
-    def AdicionarTarefa(self, tarefa):
-        self.lista.append(tarefa)
-        return True
+    def AdicionarTarefa(self, tarefa):  # Define o método AdicionarTarefa que recebe a tarefa como parâmetro
+        self.lista.append(tarefa)  # Adiciona a tarefa à lista de tarefas
+        return True  # Retorna True indicando que a tarefa foi adicionada com sucesso
 
-    def ExcluirTarefa(self, excluir):
-        self.lista.pop(excluir)
-        return True
+    def ExcluirTarefa(self, excluir):  # Define o método ExcluirTarefa que recebe o índice da tarefa a ser excluída como parâmetro
+        self.lista.pop(excluir)  # Remove a tarefa correspondente ao índice da lista de tarefas
+        return True  # Retorna True indicando que a tarefa foi excluída com sucesso
 
-    def ListarTarefas(self):
-        return self.lista
-        
+    def ListarTarefas(self):  # Define o método ListarTarefas que não recebe nenhum parâmetro
+        return self.lista  # Retorna a lista de tarefas
 
-TODO = ToDo()
+TODO = ToDo()  # Cria uma instância da classe ToDo e atribui à variável TODO
