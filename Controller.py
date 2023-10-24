3a84a4e4923fc544a18e690a33e90a781950c70d
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-10-24 16:20:12 -0300

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -8,32 +8,41 @@ class ControllerAdicionarTarefa():  # Define a classe ControllerAdicionarTarefa
         self.id = int(id)  # Converte o ID para inteiro
         self.tarefa = tarefa  # Atribui a tarefa passada como parâmetro para a instância atual
         self.tarefa = f"{self.id} - {self.tarefa}"  # Formata a tarefa com o ID e o texto da tarefa
+#?///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         if DAO.AdicionarTarefa(self.tarefa) == True:  # Se a tarefa foi adicionada com sucesso
             print("Tarefa Adicionada")  # Imprime uma mensagem de sucesso
         else:  # Se a tarefa não foi adicionada com sucesso
             print("Não foi possível adicionar a tarefa. Verifique o índice.")  # Imprime uma mensagem de erro
 
-class ControllerExcluirTarefa:  # Define a classe ControllerExcluirTarefa
-    def __init__(self, excluir):  # Método construtor que recebe o índice da tarefa a ser excluída como parâmetro
-        self.excluir = excluir  # Atribui o índice passado como parâmetro para a instância atual
-        self.excluir = int(excluir) - 1  # Converte o índice para inteiro e subtrai 1 para obter o índice correto na lista
+        
 
-        if self.excluir >= 0 and self.excluir < len(DAO.listarTarefas()):  # Se o índice é válido (está dentro do intervalo da lista)
-            tarefa = DAO.listarTarefas()[self.excluir]  # Obtém a tarefa correspondente ao índice
-            tarefa_parts = tarefa.split(" - ", 1)  # Divide a tarefa em duas partes: ID e texto da tarefa
 
-            if len(tarefa_parts) > 1:  # Se a tarefa foi dividida corretamente em duas partes
-                _, texto_tarefa = tarefa_parts  # Ignora o ID e obtém apenas o texto da tarefa
-                print(f"Excluindo a tarefa: {texto_tarefa}")  # Imprime uma mensagem informando qual tarefa está sendo excluída
-                if DAO.ExcluirTarefa(self.excluir):  # Se a tarefa foi excluída com sucesso
-                    print("Tarefa Excluída")  # Imprime uma mensagem de sucesso
-                else:  # Se a tarefa não foi excluída com sucesso
-                    print("Não foi possível excluir a tarefa. Verifique o índice.")  # Imprime uma mensagem de erro
-            else:  # Se a tarefa não foi dividida corretamente em duas partes
-                print("Tarefa não encontrada.")  # Imprime uma mensagem de erro
-        else:  # Se o índice é inválido (está fora do intervalo da lista)
-            print("Índice inválido.")  # Imprime uma mensagem de erro
+class ControllerExcluirTarefa:
+    def __init__(self, excluir):
+        self.excluir = int(excluir)
+        self.excluir_tarefa()
+
+    def excluir_tarefa(self):
+        tarefas = DAO.listarTarefas()
+
+        if self.excluir >= 1 and self.excluir <= len(tarefas):
+            tarefa = tarefas[self.excluir - 1]
+            tarefa_parts = tarefa.split(" - ", 1)
+
+            if len(tarefa_parts) > 1:
+                _, texto_tarefa = tarefa_parts
+                print(f"Excluindo a tarefa: {texto_tarefa}")
+
+                if DAO.ExcluirTarefa(self.excluir - 1):  # Correção: Chamada correta para DAO.ExcluirTarefa
+                    print("Tarefa Excluída")
+                else:
+                    print("Não foi possível excluir a tarefa. Verifique o índice.")
+            else:
+                print("Tarefa não encontrada.")
+        else:
+            print("Índice inválido.")
+
 
 class ControllerListarTarefas:  # Define a classe ControllerListarTarefas
     def __init__(self):  # Método construtor sem parâmetros
