9eb166c3a434bcf980300deccd395701584a1d38
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-10-26 08:46:28 -0300

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -6,16 +6,18 @@ class ControllerAdicionarTarefa():  # Define a classe ControllerAdicionarTarefa
     def __init__(self, tarefa):  # Método construtor que recebe a tarefa como parâmetro
         id = random.randint(1000, 9999)  # Gera um número aleatório entre 1000 e 9999 para o ID da tarefa
         self.id = int(id)  # Converte o ID para inteiro
+        self.status = "A fazer"  # Define o status da tarefa como "A fazer"
         self.tarefa = tarefa  # Atribui a tarefa passada como parâmetro para a instância atual
-        self.tarefa = f"{self.id} - {self.tarefa}"  # Formata a tarefa com o ID e o texto da tarefa
-#?///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        self.tarefa = f"{self.id} - {self.status} - {self.tarefa}"  # Concatena o ID, o status e a tarefa em uma única string
 
-        if DAO.AdicionarTarefa(self.tarefa) == True:  # Se a tarefa foi adicionada com sucesso
+        self.adicionar_tarefa()  # Chama o método adicionar_tarefa
+
+    def adicionar_tarefa(self):  # Define o método adicionar_tarefa
+        if DAO.AdicionarTarefa(self.tarefa):  # Correção: Chamada correta para DAO.AdicionarTarefa
             print("Tarefa Adicionada")  # Imprime uma mensagem de sucesso
-        else:  # Se a tarefa não foi adicionada com sucesso
-            print("Não foi possível adicionar a tarefa. Verifique o índice.")  # Imprime uma mensagem de erro
+        else:
+            print("Não foi possível adicionar a tarefa.")
 
-        
 
 
 class ControllerExcluirTarefa:
@@ -34,7 +36,7 @@ class ControllerExcluirTarefa:
                 _, texto_tarefa = tarefa_parts
                 print(f"Excluindo a tarefa: {texto_tarefa}")
 
-                if DAO.ExcluirTarefa(self.excluir - 1):  # Correção: Chamada correta para DAO.ExcluirTarefa
+                if DAO.excluirTarefa(self.excluir - 1):  # Correção: Chamada correta para DAO.ExcluirTarefa
                     print("Tarefa Excluída")
                 else:
                     print("Não foi possível excluir a tarefa. Verifique o índice.")
@@ -43,17 +45,111 @@ class ControllerExcluirTarefa:
         else:
             print("Índice inválido.")
 
+class ControllerListarTarefas:
+    def __init__(self):
+        self.lista = DAO.listarTarefas()
+        self.exibirTarefas()
 
-class ControllerListarTarefas:  # Define a classe ControllerListarTarefas
-    def __init__(self):  # Método construtor sem parâmetros
-        self.lista = DAO.listarTarefas()  # Obtém a lista de todas as tarefas e atribui para a instância atual
-        self.exibirTarefas()  # Chama o método exibirTarefas
+    def exibirTarefas(self):
+        if self.lista:
+            for i, tarefa in enumerate(self.lista, start=1):
+                tarefa_parts = tarefa.split(" - ", 2)  # Dividir em 3 partes
 
-    def exibirTarefas(self):   # Define o método exibirTarefas que imprime todas as tarefas na tela
-        for i, tarefa in enumerate(self.lista):   # Para cada índice e tarefa na lista de tarefas
-            tarefa_parts = tarefa.split(" - ", 1)   # Divide a tarefa em duas partes: ID e texto da tarefa
-            if len(tarefa_parts) > 1:   # Se a tarefa foi dividida corretamente em duas partes
-                numero_tarefa, texto_tarefa = tarefa_parts   # Obtém o ID e o texto da tarefa separadamente
-                print(f"[{i + 1}] -> {texto_tarefa}")   # Imprime o índice (incrementado em 1) e o texto da tarefa
-            else:   # Se a tarefa não foi dividida corretamente em duas partes
+                if len(tarefa_parts) == 3:
+                    _, status, texto_tarefa = tarefa_parts
+                    print(f"[{i}] - Status: {status}, Tarefa: {texto_tarefa}")
+                else:
+                    print(f"[{i}] - Tarefa não encontrada.")
+
+
+    def alterarTarefa(self, indice, nova_descricao):
+        try:
+            with open(self.arquivo, "r") as arquivo:
+                tarefas = arquivo.readlines()
+
+            if indice >= 0 and indice < len(tarefas):
+                tarefa_parts = tarefas[indice].split(" - ", 2)
+                if len(tarefa_parts) == 3:
+                    _, status, _ = tarefa_parts
+                    tarefa_parts[2] = nova_descricao  # Altera apenas a descrição
+                    tarefas[indice] = " - ".join([tarefa_parts[0], status, tarefa_parts[2]])
+
+                    with open(self.arquivo, "w") as arquivo:
+                        arquivo.writelines(tarefas)
+
+                    return True
+                else:
+                    print("Tarefa não encontrada.")
+                    return False
+            else:
+                print("Índice inválido.")
+                return False
+        except Exception as error:
+            print(error.__class__.__name__)
+            return False
+        
+
+
+
+class ControllerConcluirTarefa:
+    def __init__(self, concluir):
+        self.concluir = int(concluir)
+        self.concluir_tarefa()
+
+    def concluir_tarefa(self):
+        tarefas = DAO.listarTarefas()
+
+        if self.concluir >= 1 and self.concluir <= len(tarefas):
+            tarefa = tarefas[self.concluir - 1]
+            tarefa_parts = tarefa.split(" - ", 1)
+
+            if len(tarefa_parts) > 1:
+                _, texto_tarefa = tarefa_parts
+                print(f"Concluindo a tarefa: {texto_tarefa}")
+
+                if DAO.ConcluirTarefa(self.concluir - 1):  # Correção: Chamada correta para DAO.ConcluirTarefa
+                    print("Tarefa Concluída")
+                else:
+                    print("Não foi possível concluir a tarefa. Verifique o índice.")
+            else:
                 print("Tarefa não encontrada.")
+        else:
+            print("Índice inválido.")
+
+class ControllerListarTarefasConcluidas:
+    def __init__(self):
+        self.lista = DAO.listarTarefasConcluidas()
+        self.exibirTarefas()
+
+    def exibirTarefas(self):
+        if self.lista:
+            for i, tarefa in enumerate(self.lista, start=1):
+                tarefa_parts = tarefa.split(" - ", 2)  # Dividir em 3 partes
+
+                if len(tarefa_parts) == 3:
+                    _, status, texto_tarefa = tarefa_parts
+                    print(f"[{i}] - Status: {status}, Tarefa: {texto_tarefa}")
+                else:
+                    print(f"[{i}] - Tarefa não encontrada.")
+
+class ControllerAlterarTarefa:
+    def __init__(self, indice, nova_descricao):
+        self.indice = int(indice)
+        self.nova_descricao = nova_descricao
+        self.alterar_tarefa()
+
+    def alterar_tarefa(self):
+        if self.indice > 0:
+            tarefas = DAO.listarTarefas()
+
+            if self.indice <= len(tarefas):
+                if DAO.alterarTarefa(self.indice - 1, self.nova_descricao):
+                    print("Tarefa alterada com sucesso.")
+                else:
+                    print("Não foi possível alterar a tarefa.")
+            else:
+                print("Índice inválido.")
+        else:
+            print("Operação cancelada.")
+
+
