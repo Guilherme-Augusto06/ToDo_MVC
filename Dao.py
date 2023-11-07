20f424b1c7db82b4503dbf08429965acc145af81
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-11-06 23:16:20 -0300

    Update Dao.py

--- Dao.py
+++ Dao.py
@@ -1,8 +1,12 @@
-class Dao:  # Define a classe Dao
-    def __init__ (self):  # Método construtor sem parâmetros
-        self.arquivo  = "tarefas.txt"  # Define o nome do arquivo onde as tarefas serão armazenadas
-        with open (self.arquivo, "a") as arquivo:
-            arquivo.close()
+import os
+class Dao:
+    def __init__(self):
+        self.arquivo = "tarefas.txt"
+        if not os.path.isfile(self.arquivo) or os.path.getsize(self.arquivo) == 0:
+            # Se o arquivo não existir ou estiver vazio, adicione o cabeçalho
+            with open(self.arquivo, "w") as arquivo:
+                arquivo.write("ID   -   STATUS   - TAREFA\n")
+
 
     def AdicionarTarefa(self, tarefa):  # Define o método AdicionarTarefa que recebe a tarefa como parâmetro
         try:  # Tenta executar o bloco de código dentro do try
@@ -19,68 +23,15 @@ class Dao:  # Define a classe Dao
         
     # Define o método listar Tarefas que não recebe nenhum parâmetro
     def listarTarefas(self):
-        try:  # Tenta executar o bloco de código dentro do try
-            # Abre o arquivo em modo de leitura ("r")
+        try:
             with open(self.arquivo, "r") as arquivo:
-                # Retorna uma lista contendo todas as linhas do arquivo
                 return arquivo.readlines()
-        except Exception as error:  # Se ocorrer um erro durante a execução do bloco de código dentro do try
-            # Imprime o nome da classe do erro
+        except Exception as error:
             print(error.__class__.__name__)
-            # Retorna False indicando que as tarefas não foram listadas com sucesso
             return False
-
+        
     # Define o método excluirTarefa que recebe o índice da tarefa a ser excluída como parâmetro
-    def excluirTarefa(self, excluir):
-        try:
-            # Abre o arquivo em modo de leitura ("r")
-            with open(self.arquivo, "r") as arquivo:
-                # Lê todas as linhas do arquivo e armazena em uma lista
-                tarefas = arquivo.readlines()
-                # Remove a tarefa com o índice especificado da lista
-                tarefas.pop(excluir)
-            # Abre o arquivo em modo de escrita ("w")
-            with open(self.arquivo, "w") as arquivo:
-                # Escreve todas as tarefas da lista no arquivo
-                for tarefa in tarefas:
-                    arquivo.write(tarefa)
-            return True
-        except Exception as error:
-            print(error.__class__.__name__)
-            return False
-
-    # Define o método alterarTarefa que recebe o índice da tarefa a ser alterada e a nova descrição como parâmetros
-    def alterarTarefa(self, indice, nova_descricao):
-        try:
-            # Abre o arquivo em modo de leitura ("r")
-            with open(self.arquivo, "r") as arquivo:
-                # Lê todas as linhas do arquivo e armazena em uma lista
-                tarefas = arquivo.readlines()
-            # Verifica se o índice especificado é válido
-            if indice >= 0 and indice < len(tarefas):
-                # Extrai as partes da tarefa
-                tarefa_parts = tarefas[indice].split(" - ", 2)
-                if len(tarefa_parts) == 3:
-                    id, status, descricao = tarefa_parts  # Extrai as partes da tarefa
-                    # Atualiza somente a descrição mantendo id e status
-                    tarefa_atualizada = f"{id} - {status} - {nova_descricao}\n"
-                    tarefas[indice] = tarefa_atualizada  # Atualiza a tarefa na lista
-                    # Reescreve todas as tarefas no arquivo
-                    with open(self.arquivo, "w") as arquivo:
-                        arquivo.writelines(tarefas)
-                    return True
-                else:
-                    print("Tarefa não encontrada.")
-                    return False
-            else:
-                print("Índice inválido.")
-                return False
-        except Exception as error:
-            print(error.__class__.__name__)
-            return False
-
-    # Define o método concluirTarefa que recebe o índice da tarefa a ser concluída e o novo status como parâmetros
-    def concluirTarefa(self,indice, novo_status):
+    def excluirTarefa(self,indice, novo_status):
         try:
             # Abre o arquivo em modo de leitura ("r")
             with open(self.arquivo, "r") as arquivo:
@@ -108,6 +59,49 @@ class Dao:  # Define a classe Dao
             print(error.__class__.__name__)
             return False
 
+    # Define o método alterarTarefa que recebe o índice da tarefa a ser alterada e a nova descrição como parâmetros
+    def alterarTarefa(self, indice, novo_status):
+        try:
+            tarefas = self.listarTarefas()
+            if indice >= 0 and indice < len(tarefas):
+                tarefa_parts = tarefas[indice].split(" - ", 2)
+                if len(tarefa_parts) == 3:
+                    tarefa_parts[1] = novo_status
+                    tarefas[indice] = " - ".join(tarefa_parts)
+                    with open(self.arquivo, "w") as arquivo:
+                        arquivo.writelines(tarefas)
+                    return True
+                else:
+                    print("Tarefa não encontrada.")
+                    return False
+            else:
+                print("Índice inválido.")
+                return False
+        except Exception as error:
+            print(error.__class__.__name__)
+            return False
+
+    def concluirTarefa(self, indice, novo_status):
+        try:
+            tarefas = self.listarTarefas()
+            if indice >= 0 and indice < len(tarefas):
+                tarefa_parts = tarefas[indice].split(" - ", 2)
+                if len(tarefa_parts) == 3:
+                    tarefa_parts[1] = novo_status
+                    tarefas[indice] = " - ".join(tarefa_parts)
+                    with open(self.arquivo, "w") as arquivo:
+                        arquivo.writelines(tarefas)
+                    return True
+                else:
+                    print("Tarefa não encontrada.")
+                    return False
+            else:
+                print("Índice inválido.")
+                return False
+        except Exception as error:
+            print(error.__class__.__name__)
+            return False
+
     # Define o método adicionarTarefaConcluida que recebe a tarefa concluída como parâmetro
     def adicionarTarefaConcluida(self, tarefa_concluida):
         try:
@@ -120,7 +114,6 @@ class Dao:  # Define a classe Dao
             print(error.__class__.__name__)
             return False
 
-    # Define o método listarTarefasConcluidas que não recebe nenhum parâmetro
     def listarTarefasConcluidas(self):
         try:
             # Abre o arquivo em modo de leitura ("r")
@@ -128,11 +121,11 @@ class Dao:  # Define a classe Dao
                 # Lê todas as linhas do arquivo e armazena em uma lista
                 tarefas = arquivo.readlines()
             # Filtra as tarefas concluídas e armazena em uma nova lista
-            tarefas_concluidas = [tarefa for tarefa in tarefas if "Concluída" in tarefa]
+            tarefas_concluidas = [tarefa for tarefa in tarefas if "C" in tarefa]
             return tarefas_concluidas
         except Exception as error:
             print(error.__class__.__name__)
             return False
+    
 
-            # Cria uma instância da classe Dao e atribui à variável DAO
 DAO = Dao()
